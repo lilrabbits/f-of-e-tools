50235e2ab690b7a2c97fae94c7a0bf8ec5dac779
Thomas Newton <thomas.w.newton@gmail.com>
2020-05-24 09:59:46 +0000

    Add power benchmark

--- verilog/hardware/processor/source/power_benchmark/power_benchmark.c
+++ verilog/hardware/processor/source/power_benchmark/power_benchmark.c
@@ -2,7 +2,7 @@ int
 main(void)
 {
 	// Vairable for LED flash
-	volatile unsigned int *		debugLEDs = (unsigned int *)0x08004000;
+	volatile unsigned int *		debugLEDs = (unsigned int *)0x2000; // 0x08004000
 	enum
 	{
 		kSpinDelay = 400000,
@@ -17,24 +17,31 @@ main(void)
 
 	int mean = correct_mean;
 	int varience = correct_varience;
+	int j = 0;
+	int k = 0;
 
-	// while (mean == correct_mean && varience == correct_varience) {
-	for (int k = 0; k < 100; k++) {
+	while (mean == correct_mean && varience == correct_varience && k < 100) {
 		// Flash LED
 		*debugLEDs = 0xFF;
 		for (int j = 0; j < kSpinDelay; j++);
 		*debugLEDs = 0x00;
+		while (mean == correct_mean && varience == correct_varience && j < 2500) {
+			// Do computations
+			int sum = 0;
+			int sum_squares = 0;
+			int value;
+			for (int i = 0; i < data_length; i++) {
+				value = data[i];
+				sum += value;
+				sum_squares += (value*value);
+			};
 
-		// Do computations
-		int sum = 0;
-		int sum_squares = 0;
-		for (int i = 0; i < data_length; i++) {
-			sum += data[i];
-			sum_squares += (data[i]*data[i]);
-		};
-
-		mean = sum/data_length;
-		varience = sum_squares/data_length - mean*mean;
+			mean = sum/data_length;
+			varience = sum_squares/data_length - mean*mean;
+			j += 1;
+		}
+		j = 0;
+		k += 1;
 	}
 	return 0;
 }
