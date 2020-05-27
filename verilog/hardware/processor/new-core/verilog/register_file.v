916dfbea976de6c38bdbb6cef44940e6ad04a525
Thomas Newton <thomas.w.newton@gmail.com>
2020-05-27 18:12:09 +0000

    Shrink some oversized busses

--- verilog/hardware/processor/new-core/verilog/register_file.v
+++ verilog/hardware/processor/new-core/verilog/register_file.v
@@ -44,20 +44,20 @@
 
 
 
-module regfile(clk, write, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
-	input		clk;
-	input		write;
-	input [4:0]	wrAddr;
-	input [31:0]	wrData;
-	input [4:0]	rdAddrA;
-	output [31:0]	rdDataA;
-	input [4:0]	rdAddrB;
-	output [31:0]	rdDataB;
+module regfile #(parameter width = 32) (clk, write, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
+	input					clk;
+	input					write;
+	input [4:0]				wrAddr;
+	input [width - 1:0]		wrData;
+	input [4:0]				rdAddrA;
+	output [width - 1:0]	rdDataA;
+	input [4:0]				rdAddrB;
+	output [width - 1:0]	rdDataB;
 
 	/*
 	 *	register file, 32 x 32-bit registers
 	 */
-	reg [31:0]	regfile[31:0];
+	reg [width - 1:0]	regfile[31:0];
 
 	/*
 	 *	buffer to store address at each positive clock edge
@@ -68,11 +68,11 @@ module regfile(clk, write, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
 	/*
 	 *	registers for forwarding
 	 */
-	reg [31:0]	regDatA;
-	reg [31:0]	regDatB;
-	reg [31:0]	wrAddr_buf;
-	reg [31:0]	wrData_buf;
-	reg		write_buf;
+	reg [width -1:0]	regDatA;
+	reg [width -1:0]	regDatB;
+	reg [width -1:0]	wrAddr_buf;
+	reg [width -1:0]	wrData_buf;
+	reg					write_buf;
 
 	/*
 	 *	The `initial` statement below uses Yosys's support for nonzero
@@ -89,7 +89,7 @@ module regfile(clk, write, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
 	 *	Sets register 0 to 0
 	 */
 	initial begin
-		regfile[0] = 32'b0;
+		regfile[0] = width'b0;
 	end
 
 	always @(posedge clk) begin
@@ -105,6 +105,6 @@ module regfile(clk, write, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
 		regDatB		<= regfile[rdAddrB];
 	end
 
-	assign	rdDataA = ((wrAddr_buf==rdAddrA_buf) & write_buf & wrAddr_buf!=32'b0) ? wrData_buf : regDatA;
-	assign	rdDataB = ((wrAddr_buf==rdAddrB_buf) & write_buf & wrAddr_buf!=32'b0) ? wrData_buf : regDatB;
+	assign	rdDataA = ((wrAddr_buf==rdAddrA_buf) & write_buf & wrAddr_buf!=width'b0) ? wrData_buf : regDatA;
+	assign	rdDataB = ((wrAddr_buf==rdAddrB_buf) & write_buf & wrAddr_buf!=width'b0) ? wrData_buf : regDatB;
 endmodule
