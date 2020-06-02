78ced13c1cca7d22ac28196bfe215400b36837c4
Thomas Newton <thomas.w.newton@gmail.com>
2020-06-02 09:21:09 +0000

    Possibly fix

--- verilog/hardware/processor/new-core/verilog/data_mem.v
+++ verilog/hardware/processor/new-core/verilog/data_mem.v
@@ -247,8 +247,8 @@ module data_mem (clk, addr, write_data, memwrite, memread, sign_mask, read_data,
 				sign_mask_buf <= sign_mask;
 				
 				if(memwrite==1'b1 || memread==1'b1) begin
-					word_buf <= data_block[addr_buf_block_addr - 32'h1000];
 					clk_stall <= 1;
+					word_buf <= data_block[addr_buf_block_addr - 32'h1000];
 					if(memread_buf==1'b1) begin
 					state <= READ;
 					end
